de5c9a85948c9d10655f399cc79016084b38e977
TiagoWebMaster <tiagombferreira@hotmail.com>
2021-07-01 20:46:32 +0100

    8-puzzle solvability

--- main.c
+++ main.c
@@ -6,12 +6,28 @@ int main(int argc, char *argv[])
 {
     Node *game;
     int zero = 0;
-    char initial[10] = "ABCDE FGH\0",*aux;
+    char initial[10] = "ABCDE FGH\0",*aux,*aux2,inversions=0;
     for(aux = initial; *aux != '\0'; aux++){
         if(*aux == ' '){
             break;
         };
         zero++;
+    };
+     for(aux = initial; *aux != '\0'; aux++){
+        if(*aux == ' '){
+            continue;
+        }
+        inversions += *aux-65;
+        for(aux2 = initial; *aux2 != '\0'; aux2++){
+            printf("Estou a comparar %c ----- inversoes %d --------- %d\n",*aux,inversions,*aux); 
+            if(*aux2 < *aux){
+                /*printf("%c %c\n",*aux2,*aux);*/
+                inversions--;
+            };
+            if(*aux2 == *aux){
+                break;
+            };
+        };
     };
     game = NewNode(initial,zero,'\0');
     starter(game);
